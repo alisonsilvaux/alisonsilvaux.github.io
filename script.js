22a0ca707181cf6b9cdb7e70edd2b7a8a60beca8
alisonsilvaux <alison_if@hotmail.com>
2025-02-04 19:44:29 -0300

    A thousand changes

--- script.js
+++ script.js
@@ -7,6 +7,20 @@ document.addEventListener('DOMContentLoaded', function() {
             menuItems.forEach(i => i.classList.remove('active'));
             // Adiciona a classe active apenas no item clicado
             this.classList.add('active');
+
+            // Adiciona o scroll suave
+            if (this.getAttribute('href').startsWith('#')) {
+                e.preventDefault();
+                const targetId = this.getAttribute('href');
+                const targetElement = document.querySelector(targetId);
+                
+                if (targetElement) {
+                    targetElement.scrollIntoView({
+                        behavior: 'smooth',
+                        block: 'start'
+                    });
+                }
+            }
         });
     });
 }); 
