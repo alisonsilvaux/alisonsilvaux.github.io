b6ed1d2ed8fc34e618c272ecc4c49d6a6cecd212
alisonsilvaux <alison_if@hotmail.com>
2025-02-07 10:47:11 -0300

    Lots of changes

--- script.js
+++ script.js
@@ -3,12 +3,9 @@ document.addEventListener('DOMContentLoaded', function() {
     
     menuItems.forEach(item => {
         item.addEventListener('click', function(e) {
-            // Remove a classe active de todos os itens
             menuItems.forEach(i => i.classList.remove('active'));
-            // Adiciona a classe active apenas no item clicado
             this.classList.add('active');
 
-            // Adiciona o scroll suave
             if (this.getAttribute('href').startsWith('#')) {
                 e.preventDefault();
                 const targetId = this.getAttribute('href');
@@ -22,5 +19,21 @@ document.addEventListener('DOMContentLoaded', function() {
                 }
             }
         });
+    });
+
+    // Inicializa o efeito tilt nos cards com parâmetros mais suaves
+    VanillaTilt.init(document.querySelectorAll(".project-card"), {
+        max: 5,
+        speed: 800,
+        glare: true,
+        "max-glare": 0.1,
+        scale: 1.01,
+        perspective: 2000,
+        transition: true,
+        gyroscope: true,
+        gyroscopeMinAngleX: -5,
+        gyroscopeMaxAngleX: 5,
+        gyroscopeMinAngleY: -5,
+        gyroscopeMaxAngleY: 5
     });
 }); 
